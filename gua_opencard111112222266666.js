75ac4ab6506ac6a7e02cc2dcd11dcb572d1394bb
ygxmm <94471520+ygxmm@users.noreply.github.com>
2022-04-03 12:23:15 +0800

    Add files via upload

--- gua_opencard111112222266666.js
+++ gua_opencard111112222266666.js
@@ -310,7 +310,7 @@ async function run() {
       }
     }else{
       if($.index % 3 == 0) console.log('休息1分钟，别被黑ip了\n可持续发展')
-      if($.index % 3 == 0) await $.wait(parseInt(Math.random() * 5000 + 30000, 10))
+      if($.index % 3 == 0) await $.wait(parseInt(Math.random() * 5000 + 60000, 10))
     }
   } catch (e) {
     console.log(e)
